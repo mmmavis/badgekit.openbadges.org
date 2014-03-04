add5d8317a15d940af98820e06cea33db39b2de4
Mavis Ou <mmmavisss@gmail.com>
2014-03-03 17:05:20 -0800

    Remove duplicated event handler

--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,41 @@
 
 $(document).ready(function(){
 
+  var sendForm = function(event){
+    event.preventDefault();
+    $.ajax({
+      url: $(this).attr("action"),
+      data: $(this).serialize(),
+      type: $(this).attr('method'),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      beforeSend: function(){
+      },
+      success: function(data, textStatus, jqXHR){
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+      },
+      complete: function(jqXHR,textStatus){
+        console.log(textStatus);
+        if ( textStatus != "success" ){
+          $("#thanks-for-signup").html("Uh oh, something went wrong! Please try inputting your information again.");
+        }
+        $("#sign-up-form").hide();
+        $("#thanks-for-signup").show();
+      }
+    });
+  };
+
+  $("#sign-up-form").unbind("submit", sendForm);
+
   $("#sign-up-form input[name='EMAIL']").focus(function(){
     $("#expandable-section").slideDown(300);
   });
 
   $("#sign-up-form").keyup(function(event){
-    validateForm($(this));
+     if ( validateForm($(this)) ){
+      $("#sign-up-form").unbind("submit", sendForm).bind("submit", sendForm);
+     }
   });
 
   function validateForm(form){
@@ -22,43 +51,15 @@ $(document).ready(function(){
 
     if ( $(".invalid-field").length == 0 ){
       form.find("button[type='submit']").removeAttr("disabled");
-
-      $("#sign-up-form").submit(function(event){
-        event.preventDefault();
-        $.ajax({
-          url: $(this).attr("action"),
-          data: $(this).serialize(),
-          type: $(this).attr('method'),
-          contentType: "application/json; charset=utf-8",
-          dataType: "json",
-          beforeSend: function(){
-          },
-          success: function(data, textStatus, jqXHR){
-          },
-          error: function(jqXHR, textStatus, errorThrown){
-          },
-          complete: function(jqXHR,textStatus){
-            if ( textStatus != "success" ){
-              $("#thanks-for-signup").html("Uh oh, something went wrong! Please try inputting your information again.");
-            }
-            $("#sign-up-form").hide();
-            $("#thanks-for-signup").show();
-          }
-
-        });
-      });
+      return true;
     }else{
       return false;
     }
   }
 
-
   function validateEmail(email) {
     var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(email);
   }
-
-
-
 
 });
