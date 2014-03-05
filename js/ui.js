60eb05af4d210344df3a898323e01e03cdad2429
Mavis Ou <mmmavisss@gmail.com>
2014-03-05 11:24:47 -0800

    Fix form - data now sends to a database instead of MailChimp

--- js/ui.js
+++ js/ui.js
@@ -1,35 +1,34 @@
 
 $(document).ready(function(){
 
-
+  // on small screen, move the form to the blue section
   function checkSmallScreen(minWidth){
     if ( window.innerWidth < minWidth ){
       $("#first p").after($("#sign-up-form"));
     }
   }
-
   checkSmallScreen(600);
 
 
+  // form handler
   var sendForm = function(event){
+    var form = $("#sign-up-form");
     event.preventDefault();
     $.ajax({
-      url: $(this).attr("action"),
-      data: $(this).serialize(),
-      type: $(this).attr('method'),
-      contentType: "application/json; charset=utf-8",
+      url: form.attr("action"),
+      data: form.serialize(),
+      type: form.attr('method'),
       dataType: "json",
-      beforeSend: function(){
-      },
       success: function(data, textStatus, jqXHR){
+        var successMessage = '<h2>Thanks for signing up!</h2>' +
+                             '<p>' +
+                             'We will contact you soon.  In the meantime learn more about <a href="http://openbadges.org/">Mozilla Open Badges</a>' +
+                             '</p>';
       },
       error: function(jqXHR, textStatus, errorThrown){
+        $("#thanks-for-signup").html("<p>Uh oh, something went wrong! Please try inputting your information again.</p>");
       },
       complete: function(jqXHR,textStatus){
-        console.log(textStatus);
-        if ( textStatus != "success" ){
-          $("#thanks-for-signup").html("Uh oh, something went wrong! Please try inputting your information again.");
-        }
         $("#sign-up-form").hide();
         $("#thanks-for-signup").show();
         $("html, body").animate({ scrollTop: 0 }, "slow");
@@ -37,18 +36,24 @@ $(document).ready(function(){
     });
   };
 
-  $("#sign-up-form").unbind("submit", sendForm);
+  // if there's any missing field, unbind the submit handler
+  if (  !validateForm( $("#sign-up-form") )  ){
+    $("#sign-up-form").unbind("submit", sendForm);
+  }
 
+  // expand the form when the e-mail field is in focus
   $("#sign-up-form input[name='EMAIL']").focus(function(){
     $("#expandable-section").slideDown(300);
   });
 
+  // validate the from as user types
   $("#sign-up-form").keyup(function(event){
      if ( validateForm($(this)) ){
       $("#sign-up-form").unbind("submit", sendForm).bind("submit", sendForm);
      }
   });
 
+  // form validator
   function validateForm(form){
     var fields = form.find("input");
     $.each(fields,function(i,field){
